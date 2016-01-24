2b44b99aa6d8bab8cf8ce2b3475cbba6bbb6fd29
skwu6543 <skwu6543@gmail.com>
2016-01-24 17:47:55 +0800

    test

---
+++ 111.R
@@ -0,0 +1,12 @@
+git config --list
+git config --global user.name "skwu6543"
+git config --global user.email "skwu6543@gmail.com"
+
+git clone "https://github.com/skwu6543"
+cd
+
+https://github.com/skwu6543
+
+https://github.com/yaojenkuo/NTUTrainRL2
+
+git clone https://github.com/yaojenkuo/NTUTrainRL1
